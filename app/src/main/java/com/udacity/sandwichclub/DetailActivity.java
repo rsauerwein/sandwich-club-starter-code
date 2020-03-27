be30e7c3d3effd5e0c61f8db775ef44fea9f9550
Rene Sauerwein <rene@sauerwein.cc>
2020-03-27 08:55:18 +0100

    Add populateUI doc

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -55,6 +55,7 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     /**
+     * Adds all Sandwich information to the view
      * @param sandwich
      */
     private void populateUI(Sandwich sandwich) {
